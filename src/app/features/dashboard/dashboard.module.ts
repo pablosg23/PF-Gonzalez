45d6c26aee0295a110bb833b62954ff97d0555db
pablosg23 <pablosg23@gmail.com>
2024-10-08 19:19:23 -0300

    add students module

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { StudentsModule } from "./students/students.module";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    StudentsModule
   ]
 })
 export class DashboardModule { }

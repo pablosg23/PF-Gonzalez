d79c60fb513a25a932c3647eccfe249b625d7ef0
pablosg23 <pablosg23@gmail.com>
2024-10-08 21:15:00 -0300

    add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { DashboardModule } from "./features/dashboard/dashboard.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { DashboardModule } from "./features/dashboard/dashboard.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DashboardModule
+    DashboardModule,
+    NgbModule
   ],
   providers: [
     provideAnimationsAsync()

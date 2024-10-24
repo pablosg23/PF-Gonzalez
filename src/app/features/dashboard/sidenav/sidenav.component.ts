c3fdd98f82842733e26c1412aee72cd65b456eeb
pablosg23 <pablosg23@gmail.com>
2024-10-23 21:56:29 -0300

    add logout logic using Router

--- src/app/features/dashboard/sidenav/sidenav.component.ts
+++ src/app/features/dashboard/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sidenav',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
+  constructor(
+    private router: Router
+  ) {
+  }
 
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/auth/login'])
+  }
 }

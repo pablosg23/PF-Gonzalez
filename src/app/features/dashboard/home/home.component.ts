5d38e230984686d3fd5527e3644d37c4bda5872a
pablosg23 <pablosg23@gmail.com>
2024-11-17 02:14:11 -0300

    add home page details

--- src/app/features/dashboard/home/home.component.ts
+++ src/app/features/dashboard/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../../core/services/auth.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(
+    public authService: AuthService
+  ) {
+  }
 
 }
